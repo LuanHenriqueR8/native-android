cedd03e373676792f46e3124424d8847480c765d
Luan Henrique <luanhenriquer8@gmail.com>
2018-12-14 10:33:41 -0200

    Generating a random number

--- randon-number/app/src/main/java/me/jogandoecodando/randomnumber/RandomNumberActivity.java
+++ randon-number/app/src/main/java/me/jogandoecodando/randomnumber/RandomNumberActivity.java
@@ -2,6 +2,10 @@ package me.jogandoecodando.randomnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class RandomNumberActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class RandomNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_number);
+    }
+
+    private Integer generateARandromNumber(){
+        return new Random().nextInt(10);
+    }
+
+    public void updateResultLabel(View view){
+        TextView textView = findViewById(R.id.resultLabel);
+        Integer result = generateARandromNumber();
+        textView.setText("The selected number was: " + result);
     }
 }
