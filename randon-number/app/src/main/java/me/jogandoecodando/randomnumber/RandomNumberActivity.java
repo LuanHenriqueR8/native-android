0abb8a31054dd4e9dfbe2c915d5a177e44a7ee3e
Luan Henrique <luanhenriquer8@gmail.com>
2018-12-14 10:10:34 -0200

    first commit

---
+++ randon-number/app/src/main/java/me/jogandoecodando/randomnumber/RandomNumberActivity.java
@@ -0,0 +1,13 @@
+package me.jogandoecodando.randomnumber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RandomNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_number);
+    }
+}
